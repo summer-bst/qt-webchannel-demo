7bbff74f95133d24755831b851ce48c1ddd2417a
summer-bst <summer@bluestacks.com>
2022-11-03 20:17:33 +0800

    fix:修改默认包名
    (cherry picked from commit 6a5c205e9cafa128713988c15883eb1fa5d1dd77)

--- src/components/openApp/mock.ts
+++ src/components/openApp/mock.ts
@@ -9,9 +9,9 @@ const cloudApp: ICloudApp = {
 };
 
 const androidApp: IAndroidApp = {
-  package_name: "com.plarium.raidlegends",
+  package_name: "com.wt.cryptornado.idle.rpg",
   icon_url:
-    "https://cloud.bluestacks.com/app/icon?pkg=com.plarium.raidlegends&use_cdn=true&w=300",
+    "https://cloud.bluestacks.com/app/icon?pkg=com.wt.cryptornado.idle.rpg&use_cdn=true&w=300",
   game_name: "RAID: Shadow Legends",
   action: "",
   action_value: "",
@@ -25,7 +25,7 @@ const configData = {
   cloudApp: JSON.stringify(cloudApp, null, 4),
   androidApp: JSON.stringify(androidApp, null, 4),
   pcApp: JSON.stringify(pcApp, null, 4),
-  androidAppPure: JSON.stringify("com.plarium.raidlegends", null, 4),
+  androidAppPure: JSON.stringify("com.wt.cryptornado.idle.rpg", null, 4),
 };
 
 export default configData;
